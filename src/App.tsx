4ed21d32ef4aab6b55ebc12e8c6632776a941e51
substro <prof.aliamin@gmail.com>
2023-12-01 00:14:28 +0200

    ready for developement

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Button from "@mui/material/Button";
+
 function App() {
-	return <></>;
+	return <Button variant="contained">Hello world</Button>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -6,8 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<NextUIProvider>
-			<App />
-		</NextUIProvider>
+		<App />
 	</React.StrictMode>
 );
